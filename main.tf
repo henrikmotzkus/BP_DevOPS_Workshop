b4ba473dc5716f2da1c80f638e9ed5aacf1044d8
henrikmotzkus <henrik@motzkus.info>
2024-09-25 17:50:01 +0200

    backend tages magic

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.3.0"
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "4.3.0"
+        }
     }
-    }
-    backend "remote" {
+
+    backend "azurerm" {
         resource_group_name  = "BP_DevOPS-Workshop"
         storage_account_name = "henrikbpdevopsworkshop"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
         use_oidc             = true
     }
-
 }
 
 provider "azurerm" {
