e52f3fa39a721ffb999ff9ebbd07c61b6ede604d
henrikmotzkus <henrik@motzkus.info>
2024-09-25 13:54:19 +0200

    Changed from AVM to plain TF

--- tf-2-vnet.tf
+++ tf-2-vnet.tf
@@ -1,17 +1,22 @@
-module "avm-res-network-virtualnetwork" {
-  source = "Azure/avm-res-network-virtualnetwork/azurerm"
-  address_space      = ["10.0.0.0/16"]
-  location            = "North Europe"
-  name                = "VNET-HUB"
-  resource_group_name = var.resource_group_name
-  subnets = {
-    "subnet1" = {
-      name             = "snet-1"
-      address_prefixes = ["10.0.0.0/24"]
-    }
-    "subnet2" = {
-      name             = "snet-2"
-      address_prefixes = ["10.0.1.0/24"]
-    }
-  }
+resource "azurerm_virtual_network" "vnet" {
+  name                  = "VNET-Hub"
+  location              = azurerm_resource_group.rghubnetworking.location
+  resource_group_name   = azurerm_resource_group.rghubnetworking.name
+  address_space         = ["10.0.0.0/16"]
+  #dns_servers           = ["10.0.0.4", "10.0.0.5"]
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "gatewaysubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.rghubnetworking.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "firewallsubnet" {
+  name                 = "AzureFirewallSubnet-subnet"
+  resource_group_name  = azurerm_resource_group.rghubnetworking.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
---
+++ tf-3-fw.tf
@@ -0,0 +1,21 @@
+resource "azurerm_public_ip" "fwpubip" {
+  name                = "testpip"
+  location            = azurerm_resource_group.rghubnetworking.location
+  resource_group_name = azurerm_resource_group.rghubnetworking.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "fw" {
+  name                = "testfirewall"
+  location            = azurerm_resource_group.rghubnetworking.location
+  resource_group_name = azurerm_resource_group.rghubnetworking.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.firewallsubnet.id
+    public_ip_address_id = azurerm_public_ip.fwpubip.id
+  }
+}
